25b6a28e0b4bc3189599c1fd5db156de605b62af
Morteza <mtoshe87@gmail.com>
2022-07-23 00:25:23 +0430

    footer updated!

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => {
     <>
       <footer id="footer" className="footer-copyright">
         <small>&copy; Morteza Toushekhah, all rights reserved!</small>
+        <small><a href="https://github.com/mortezatoushekhah/my-website">Source Code</a></small>
         <a href="#">
           <FontAwesomeIcon className="top-arrow" icon={faArrowAltCircleUp} />
         </a>
